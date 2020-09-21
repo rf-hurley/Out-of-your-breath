5e7d7ddb1f36bcba5471af738f9ce06d1bd3f675
rf-hurley <70685246+rf-hurley@users.noreply.github.com>
2020-09-21 15:44:23 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 //create map from leaflet.js
-const mymap = L.map('mapid').setView([39.5, -98.5], 6);
+const mymap = L.map('mapid').setView([39.5, -98.5], 4);
 //add a tile layer from OpenStreetMaps
-const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 tiles.addTo(mymap);
 //play and universal volume
